cec95ea4895bf7558d519a99cb5a6f742ec112a5
Octcarp <octcarp@hotmail.com>
2022-11-23 01:54:42 +0800

    Update string fast read template
    for char array and std::string

--- #_template_c-cpp.cpp
+++ #_template_c-cpp.cpp
@@ -2,7 +2,7 @@
 
 #define ll long long
 
-//fast integer read & write
+// fast integer read & write
 int read() {
     int s = 0, f = 1;
     char ch = getchar();
@@ -11,7 +11,7 @@ int read() {
         ch = getchar();
     }
     while (ch >= '0' && ch <= '9') {
-        s = s * 10 + ch - '0';
+        s = (s << 1) + (s << 3) + (ch ^ '0');
         ch = getchar();
     }
     return s * f;
@@ -29,11 +29,11 @@ void write(int x) {
     return;
 }
 
-void disable() { //C++ disables the io synchronization
+void disable() { // C++ disables the io synchronization
     std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
 }
 
-//fast long readL & writeL
+// fast long readL & writeL
 ll readL() {
     ll s = 0, f = 1;
     char ch = getchar();
@@ -42,7 +42,7 @@ ll readL() {
         ch = getchar();
     }
     while (ch >= '0' && ch <= '9') {
-        s = s * 10 + ch - '0';
+        s = (s << 1) + (s << 3) + (ch ^ '0');
         ch = getchar();
     }
     return s * f;
@@ -58,4 +58,32 @@ void writeL(ll x) {
         putchar(x % 10 + '0');
     } else putchar(x + '0');
     return;
+}
+
+// fast char array read, begin from str[0]
+int readChs(char str[]) {
+    int len = -1;
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n' || ch == '\r') {
+        ch = getchar();
+    }
+    while (ch != ' ' && ch != '\n' && ch != '\r') {
+        str[++len] = ch;
+        ch = getchar();
+    }
+    return len;
+}
+
+// "fast string" read
+std::string readStr() {
+    std::string str;
+    char s = getchar();
+    while (s == ' ' || s == '\n' || s == '\r') {
+        s = getchar();
+    }
+    while (s != ' ' && s != '\n' && s != '\r') {
+        str += s;
+        s = getchar();
+    }
+    return str;
 }
